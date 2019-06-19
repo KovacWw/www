90cad1cd2c32f5494ffb1c99e977c9b7f627c4a6
root <root@ip-10-110-1-78.ap-northeast-1.compute.internal>
2019-06-19 09:04:52 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ mv /tmp/ww/main /main
 mv /tmp/ww/handle.conf /etc/supervisor/conf.d/handle.conf
 mv /tmp/ww/templates /templates
 
-echo "2019/6/19 16:57"
+echo "last commit: 2019/6/19 16:57"
 
 supervisord -c "/etc/supervisor/supervisord.conf"
 supervisorctl start all
