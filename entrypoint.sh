b5ff0f039da023827e978c34d55167334c1290f6
yichen wu <kovac.wu@binance.com>
2019-07-08 16:05:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ mv /tmp/ww/main /main
 mv /tmp/ww/handle.conf /etc/supervisor/conf.d/handle.conf
 mv /tmp/ww/templates /templates
 
-echo "last commit: 2019/6/19 17:31"
+echo "last commit: 2019/7/8 16:00"
 
 supervisord -c "/etc/supervisor/supervisord.conf"
 supervisorctl start all
